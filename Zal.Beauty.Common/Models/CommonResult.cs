cbae6fc2d9f4c70f1cf987cefa4ff64e4e123657
XIANWEI HU <820693371@qq.com>
2017-12-20 22:43:33 +0800

    创建项目结构

---
+++ Zal.Beauty.Common/Models/CommonResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zal.Beauty.Base.Models
+{
+    /// <summary>
+    /// 公共结果对象
+    /// </summary>
+    public class CommonResult
+    {
+    }
+}
