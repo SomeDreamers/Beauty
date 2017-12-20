4c6468ef5d8b3dedf498d74553521f2d0dd6d150
XIANWEI HU <820693371@qq.com>
2017-12-20 23:33:32 +0800

    增加数据库连接

---
+++ Zal.Beauty.Core/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zal.Beauty.Core
+{
+    public class ApplicationDbContext : DbContext
+    {
+        /// <summary>
+        /// 用户表
+        /// </summary>
+        //public DbSet<User> User { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- Zal.Beauty.WebApp/Controllers/HomeController.cs
+++ Zal.Beauty.WebApp/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Beauty.Controllers
+namespace Zal.Beauty.WebApp.Controllers
 {
     public class HomeController : Controller
     {
