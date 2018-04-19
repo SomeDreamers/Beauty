1c74744c1126eed67ff752dbc62cac5901f55a2d
XIANWEI HU <820693371@qq.com>
2018-04-19 20:43:02 +0800

    添加商城模块对象

---
+++ src/Zal.Beauty.Core/ORM/Malls/ProductImg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Zal.Beauty.Interface.Enums.Malls;
+
+namespace Zal.Beauty.Core.ORM.Malls
+{
+    /// <summary>
+    /// 商品图片
+    /// </summary>
+    [Table("mall_product_imgs")]
+    public class ProductImg
+    {
+        // <summary>
+        /// 主键
+        /// </summary>
+        [Column("id")]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 商品ID
+        /// </summary>
+        [Column("product_id")]
+        public long ProductId { get; set; }
+
+        /// <summary>
+        /// 图片ID
+        /// </summary>
+        [Column("img_id")]
+        public long ImgId { get; set; }
+
+        /// <summary>
+        /// 商品图片类型
+        /// </summary>
+        [Column("type")]
+        public EProductImgType Type { get; set; }
+    }
+}
---
+++ src/Zal.Beauty.Core/ORM/Malls/SpecificationValue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Zal.Beauty.Core.ORM.Malls
+{
+    /// <summary>
+    /// 规格值
+    /// </summary>
+    [Table("mall_specification_values")]
+    public class SpecificationValue
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        [Column("id")]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 规格ID
+        /// </summary>
+        [Column("ispecification_idd")]
+        public long SpecificationId { get; set; }
+
+        /// <summary>
+        /// 规格值名
+        /// </summary>
+        [Column("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 是否删除
+        /// </summary>
+        [Column("is_del")]
+        public bool IsDel { get; set; }
+    }
+}
