30b970004f8301f6b89640212ef155e262785409
baiyang <Administrator@WINDOWS-3142256>
2018-05-13 23:22:38 +0800

    图片选择模态框，商品分类

---
+++ src/Zal.Beauty.WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zal.Beauty.Interface.IManager.Malls;
+
+namespace Zal.Beauty.WebApp.Controllers
+{
+    /// <summary>
+    /// 微信商品分类
+    /// </summary>
+    public class CategoryController:Controller
+    {
+            public readonly ICategoryManager categoryManager;
+            public CategoryController(ICategoryManager categoryManager)
+            {
+                this.categoryManager = categoryManager;
+            }
+
+            /// <summary>
+            /// 微信商品分类主页
+            /// </summary>
+            /// <returns></returns>
+            public async Task<IActionResult> Index()
+            {
+            //var model = await categoryManager.GetCategoryAllInfosAsync();
+            //return View(model);
+            return null;
+            }
+
+        }
+}
